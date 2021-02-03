cbbe566e7761b0543854ecb3593966d4a9f9b135
vanndhall <szymon.mantey@wp.pl>
2021-02-03 21:30:12 +0100

    add files into repository

--- SimpleAsync/MainWindow.xaml.cs
+++ SimpleAsync/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,113 @@ namespace SimpleAsync
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void executeSync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            RunDownloadSync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: {elapsedMs}";
+
+        }
+       
+        private async void executeAsync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            //await RunDownloadASync();       // wolniejsze ale po kolei wysweitla wyniki
+            await RunDownloadParallelASync(); //szybsze ale wyswietla wszystkie wyniki na raz
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: {elapsedMs}";
+        }
+
+        
+
+        private List<string> PrepData()
+        {
+            List<string> output = new List<string>();
+            resultsWindow.Text = "";
+
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.wp.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+            output.Add("https://www.sadistic.pl");
+
+            return output;
+        }
+
+        private void RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                ReportWebsiteInfo(results);
+            }
+        }
+        // działa tak szybko jak metoda Sync() - podobne czasy
+        private async Task RunDownloadASync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = await Task.Run(() =>DownloadWebsite(site));
+                ReportWebsiteInfo(results);
+            } 
+        }
+
+        //bardziej zoptymalizowana metoda Async która jest 3 razy szybsza od Sync()
+        private async Task RunDownloadParallelASync()
+        {
+            List<string> websites = PrepData();
+            List<Task < WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            foreach (string site in websites)
+            {
+               // tasks.Add(Task.Run(() => DownloadWebsite(site))); //wolniejsze metoda DownloadWebsite bez Async
+                tasks.Add(DownloadWebsiteAsync(site)); //szybsze metoda DownloadWebsite z Async - niepotrzebny Task.Run
+            }
+
+            var results = await Task.WhenAll(tasks);
+
+            foreach (var item in results)
+            {
+                ReportWebsiteInfo(item); 
+            }
+        }
+
+        private WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+            return output;
+        }
+
+        private async Task<WebsiteDataModel> DownloadWebsiteAsync(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = await client.DownloadStringTaskAsync(websiteURL);
+            return output;
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            resultsWindow.Text += $"{data.WebsiteUrl} downloaded: {data.WebsiteData.Length} characters log.{ Environment.NewLine}";
         }
     }
 }
---
+++ SimpleAsync/WebsiteDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAsync
+{
+    public class WebsiteDataModel
+    {
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+}
